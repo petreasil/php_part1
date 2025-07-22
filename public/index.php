70f4237528e28781e6ff17480f65174617156a7e
Silviu Petrea <silviu.petrea@evozon.com>
2025-07-22 18:41:41 +0300

    update tool1

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-phpinfo();
+//phpinfo();
+
+require_once __DIR__.'/../vendor/autoload.php';
+use Root\App\Tools\tool1;
+
+$inputFile = 'input.csv';
+$outputFile = 'output.csv';
+$headerRow = [ 'TASK', 'NAME', 'EMAIL', 'STATUS', 'CREATED_AT', 'UPDATED_AT', 'DELETED_AT',"FILED1","FILED2"];
+try {
+    $tool1 = new tool1($inputFile, $outputFile, $headerRow);
+    $tool1->prependHeader();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+echo "Done";
