80848e1cc95e8647fda2d529464ea804cc592315
Silviu Petrea <silviu.petrea@evozon.com>
2025-07-23 09:14:18 +0300

    add reoreder columns

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-phpinfo();
+// phpinfo();
 
 require_once __DIR__.'/../vendor/autoload.php';
 use Root\App\Tools\tool1;
@@ -8,10 +8,17 @@ use Root\App\Tools\tool1;
 $inputFile = 'input.csv';
 $outputFile = 'output.csv';
 $headerRow = [ 'TASK', 'NAME', 'EMAIL', 'STATUS', 'CREATED_AT', 'UPDATED_AT', 'DELETED_AT',"FILED1","FILED2"];
+$tool1 = new tool1();
 try {
-    $tool1 = new tool1();
     $tool1->prependHeaderRow($inputFile, $outputFile, $headerRow);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
+ echo 'Done';
 
+//  try {
+//     $tool1->addIndexingColumn($inputFile, $outputFile, $headerRow);
+// } catch (Exception $e) {
+//     echo $e->getMessage();
+// }
+//  echo 'Done index column';
--- src/Tools/tool1.php
+++ src/Tools/tool1.php
@@ -3,7 +3,7 @@ namespace Root\App\Tools;
 
 class tool1
 {
-   public function prependHeaderRow($inputFile, $outputFile, $headerRow)
+    public function prependHeaderRow($inputFile, $outputFile, $headerRow)
     {
         $fpIn = fopen($inputFile, 'r');
         $fpOut = fopen($outputFile, 'w');
@@ -16,8 +16,54 @@ class tool1
             fputcsv($fpOut, $row, ',', '"', '\\');
         }
 
+        fclose($fpIn);
+        fclose($fpOut);
+        return $this;
+    }
+
+    public function addIndexingColumn($inputFile, $outputFile, $indexName)
+    {
+        $fpIn = fopen($inputFile, 'r');
+        $fpOut = fopen($outputFile, 'w');
+
+        $index = 1;
+        while (($row = fgetcsv($fpIn, 0, ',', '"', '\\')) !== FALSE) {
+            // Add the indexing column
+            array_unshift($row, $index);
+            fputcsv($fpOut, $row, ',', '"', '\\');
+            $index++;
+        }
 
         fclose($fpIn);
         fclose($fpOut);
+        return $this;
+    }
+
+    public function reorderColumns($inputFile, $outputFile, $newColumnOrder)
+    {
+        $csv = [];
+        if (($handle = fopen($inputFile, 'r')) !== FALSE) {
+            $header = fgetcsv($handle, 1000, ',');
+            while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
+                $csv[] = array_combine($header, $data);
+            }
+            fclose($handle);
+        }
+
+        $output = [];
+        foreach ($csv as $row) {
+            $newRow = [];
+            foreach ($newColumnOrder as $column) {
+                $newRow[$column] = $row[$column];
+            }
+            $output[] = $newRow;
+        }
+
+        $fp = fopen($outputFile, 'w');
+        fputcsv($fp, $newColumnOrder);
+        foreach ($output as $row) {
+            fputcsv($fp, $row);
+        }
+        fclose($fp);
     }
 }
