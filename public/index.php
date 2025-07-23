4cbecff2786edc917322a19a92df9d59ba06d9a3
Silviu Petrea <silviu.petrea@evozon.com>
2025-07-23 08:54:02 +0300

    add insert column

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//phpinfo();
+phpinfo();
 
 require_once __DIR__.'/../vendor/autoload.php';
 use Root\App\Tools\tool1;
@@ -9,10 +9,9 @@ $inputFile = 'input.csv';
 $outputFile = 'output.csv';
 $headerRow = [ 'TASK', 'NAME', 'EMAIL', 'STATUS', 'CREATED_AT', 'UPDATED_AT', 'DELETED_AT',"FILED1","FILED2"];
 try {
-    $tool1 = new tool1($inputFile, $outputFile, $headerRow);
-    $tool1->prependHeader();
+    $tool1 = new tool1();
+    $tool1->prependHeaderRow($inputFile, $outputFile, $headerRow);
 } catch (Exception $e) {
     echo $e->getMessage();
 }
 
-echo "Done";
--- src/Tools/tool1.php
+++ src/Tools/tool1.php
@@ -1,95 +1,23 @@
 <?php
 namespace Root\App\Tools;
-use Exception;
 
 class tool1
 {
-    private string $inputFile;
-    private string $outputFile;
-    private array $headerRow;
-
-    public function __construct(string  $inputFile, string  $outputFile, array $headerRow)
+   public function prependHeaderRow($inputFile, $outputFile, $headerRow)
     {
-        $this->inputFile = $inputFile;
-        $this->outputFile = $outputFile;
-        $this->headerRow = $headerRow;
-    }
+        $fpIn = fopen($inputFile, 'r');
+        $fpOut = fopen($outputFile, 'w');
 
-    public function prependHeader(): void
-    {
-        $inputHandle = fopen($this->inputFile, 'r');
-        if (!$inputHandle) {
-            throw new Exception('Failed to open input file');
+        // Write the header row
+        fputcsv($fpOut, $headerRow, ',', '"', '\\');
+
+        // Copy the rest of the file
+        while (($row = fgetcsv($fpIn, 0, ',', '"', '\\')) !== FALSE) {
+            fputcsv($fpOut, $row, ',', '"', '\\');
         }
 
-        $outputHandle = fopen($this->outputFile, 'w');
-        if (!$outputHandle) {
-            throw new Exception('Failed to open output file');
-        }
 
-        // Write the header row to the output file
-        fputcsv($outputHandle, $this->headerRow, ',', '"', '\\');
-
-        // Read the input file and write to the output file
-        while (($data = fgetcsv($inputHandle, 0, ',', '"', '\\')) !== FALSE) {
-            fputcsv($outputHandle, $data, ',', '"', '\\');
-        }
-
-        fclose($inputHandle);
-        fclose($outputHandle);
-    }
-
-    public function addIndexColumn(
-        string $indexColumnName = 'ID',
-        int $startIndex = 1,
-        int $increment = 1,
-        string $position = 'start'
-    ): bool {
-        try {
-            if (!file_exists($this->inputFile)) {
-                throw new Exception("Input file does not exist: {$this->inputFile}");
-            }
-
-            $inputHandle = fopen($this->inputFile, 'r');
-            $outputHandle = fopen($this->outputFile, 'w');
-
-            if (!$inputHandle || !$outputHandle) {
-                throw new Exception("Unable to open file handles");
-            }
-
-            $currentIndex = $startIndex;
-            $isFirstRow = true;
-
-            while (($row = fgetcsv($inputHandle, 0, ',', '"', '\\')) !== false) {
-                if ($isFirstRow ) {
-                    // Add index column header
-                    $newRow = $position === 'start'
-                        ? array_merge([$indexColumnName], $row)
-                        : array_merge($row, [$indexColumnName]);
-                } else {
-                    // Add index value
-                    $indexValue =  $isFirstRow ? $indexColumnName : $currentIndex;
-                    $newRow = $position === 'start'
-                        ? array_merge([$indexValue], $row)
-                        : array_merge($row, [$indexValue]);
-
-                    if (!$isFirstRow) {
-                        $currentIndex += $increment;
-                    }
-                }
-
-                fputcsv($outputHandle, $newRow, 0, ',', '"', '\\');
-                $isFirstRow = false;
-            }
-
-            fclose($inputHandle);
-            fclose($outputHandle);
-
-            return true;
-
-        } catch (Exception $e) {
-            error_log("CSV Index Manager Error: " . $e->getMessage());
-            return false;
-        }
+        fclose($fpIn);
+        fclose($fpOut);
     }
 }
