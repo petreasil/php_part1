292391da61c91905d1699599de5a8cba996af794
Silviu Petrea <silviu.petrea@evozon.com>
2025-07-30 07:39:36 +0300

    add task1 req1

--- src/Controller/MyController.php
+++ src/Controller/MyController.php
@@ -3,6 +3,8 @@
 namespace Silviu\CsvTools\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +16,24 @@ final class MyController extends AbstractController
         return $this->render('my/index.html.twig', [
             'controller_name' => 'MyController',
         ]);
+    }
+    #[Route('/test', name: 'app_test')]
+    public function jsonroute(): JsonResponse
+    {
+        $data = ['message' => 'This is a JSON response from /test',
+            'status' => 'success',
+            'timestamp' => (new \DateTime())->format('Y-m-d H:i:s')];
+        return new JsonResponse($data);
+
+    }
+    #[Route('/test/{id}', name: 'app_single',requirements: ['id' => '\d+'])]
+    public function test_params(int $id): Response
+    {
+        if($id === 2){
+            sleep(2);
+            return new RedirectResponse("https://google.com",);
+        }
+        return new Response("Wait");
+
     }
 }
